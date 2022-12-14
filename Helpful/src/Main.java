77ed7c0b399980a0f8c4488889415934701dae29
Greycell <dominik.severin@yandex.com>
2022-12-14 14:51:00 +0100

    Update

--- Helpful/src/Main.java
+++ Helpful/src/Main.java
@@ -1,13 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+
         // Erstellung eines Objekts der eigenen TestClass "new TestClass() = Constructor"
         TestClass a = new TestClass();
-        // Zuweisung von NICHT-Statischen Werten des Objekts
+        // Zuweisung von NICHT-Statischen Werten des Objekts (gelten nur für dieses Objekt)
         a.id = 30;
         a.name = "Master";
         a.istAn = false;
         // Ausführung einer NICHT-Statischen Methode über das Objekt
         a.printTextObject();
+
         // Zuweisung von Statischen Werten der Klasse (Kein Objekt nötig)
         TestClass.ID = 10;
         TestClass.NAME = "Mister";
