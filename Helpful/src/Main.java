3d9247289c02d67cba9891db439442279930dc34
Greycell <dominik.severin@yandex.com>
2022-12-14 15:28:14 +0100

    Update

--- Helpful/src/Main.java
+++ Helpful/src/Main.java
@@ -2,19 +2,35 @@ public class Main {
     public static void main(String[] args) {
 
         // Erstellung eines Objekts der eigenen TestClass "new TestClass() = Constructor"
-        ObjektClass a = new ObjektClass();
+        ObjektClass a = new ObjektClass(10, "Master", false);
+        ObjektClass b = new ObjektClass(15, "Hallo");
         // Zuweisung von NICHT-Statischen Werten des Objekts (gelten nur für dieses Objekt)
-        a.id = 30;
-        a.name = "Master";
-        a.istAn = false;
         // Ausführung einer NICHT-Statischen Methode über das Objekt
-        a.printTextObject();
+        //a.printTextObject();
 
         // Zuweisung von Statischen Werten der Klasse (Kein Objekt nötig)
         ObjektClass.ID = 10;
         ObjektClass.NAME = "Mister";
         ObjektClass.istAN = true;
         // Ausführung einer Statischen Methode über die Klasse (Kein Objekt nötig)
-        ObjektClass.printTextStatic();
+        //ObjektClass.printTextStatic();
+        printNumber(10);
+        printNumber(2.5f);
+        printNumber(15, 20);
+    }
+    public static void printNumber(int a)
+    {
+        System.out.println(a);
+        System.out.println("Int methode");
+    }
+    public static void printNumber(int a, int b)
+    {
+        System.out.println(a +" "+ b);
+        System.out.println("Int methode 2");
+    }
+    public static void printNumber(float a)
+    {
+        System.out.println(a);
+        System.out.println("Float methode");
     }
 }
--- Helpful/src/ObjektClass.java
+++ Helpful/src/ObjektClass.java
@@ -5,6 +5,18 @@ public class ObjektClass {
     static int ID;
     static String NAME;
     static boolean istAN;
+    ObjektClass(int id, String name)
+    {
+        this.id = id;
+        this.name = name;
+    }
+    ObjektClass(int id, String name, boolean istAn)
+    {
+        this.id = id;
+        this.name = name;
+        this.istAn = istAn;
+    }
+
     public void printTextObject()
     {
         System.out.println("ID: " + id + " Name: " + name + " An:" + istAn );
