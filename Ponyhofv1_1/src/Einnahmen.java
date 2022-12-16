d03b7d4e5b2ac12e449abba36eaab754c6d672cc
Greycell <dominik.severin@yandex.com>
2022-12-16 12:46:43 +0100

    Ponyhof v1.1

---
+++ Ponyhofv1_1/src/Einnahmen.java
@@ -0,0 +1,9 @@
+public class Einnahmen {
+    static void einnahmenAusgabe()
+    {
+        Besucher.ausgabeGesamt();
+        Verpflegung.ausgabeGesamt();
+        Attraktion.ausgabeGesamt();
+        Besucher.einnahmenGesamt();
+    }
+}
--- Ponyhofv1_1/src/Main.java
+++ Ponyhofv1_1/src/Main.java
@@ -1,9 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         Besucher.erstelleBesucherliste();
-        Besucher.ausgabeGesamt();
-        Verpflegung.ausgabeGesamt();
-        Attraktion.ausgabeGesamt();
-        Besucher.einnahmenGesamt();
+        Einnahmen.einnahmenAusgabe();
     }
 }
