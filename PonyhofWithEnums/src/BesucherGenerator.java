0f4a8dc5cce5e307725baf090c4c6817645ff414
Grey Cell <Grey Cell@DESKTOP-H0RQKAN>
2022-12-18 21:19:09 +0100

    Ponyhof updated

--- PonyhofWithEnums/src/BesucherGenerator.java
+++ PonyhofWithEnums/src/BesucherGenerator.java
@@ -3,9 +3,4 @@ import java.util.List;
 import java.util.Random;
 
 public class BesucherGenerator {
-    List<Object> list = new ArrayList<>();
-    static Random rnd = new Random();
-    public static void besucherGenerator() {
-
-    }
 }
--- Ponyhofv1_1/src/Kleinkind.java
+++ Ponyhofv1_1/src/Kleinkind.java
@@ -17,13 +17,7 @@ public class Kleinkind extends Besucher{
         kleinkind++;
         for(int i=0;i<3;i++) Attraktion.liste.add(new Attraktion(rnd.nextInt(0, 5)));
         for(int i=0;i<rnd.nextInt(2, 5);i++)Verpflegung.liste.add(new Verpflegung(rnd.nextInt(0, 8)));
-        if(rnd.nextInt(0, 2) == 0)
-        {
-            Besucher.liste.add(new Erwachsener());
-        }
-        else
-        {
-            Besucher.liste.add(new Rentner());
-        }
+        if(rnd.nextInt(0, 2) == 0) Besucher.liste.add(new Erwachsener());
+        else Besucher.liste.add(new Rentner());
     }
 }
