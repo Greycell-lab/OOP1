98bf2473a7172aa11fdbcfb9373ee481b8eb9401
Grey Cell <Grey Cell@DESKTOP-H0RQKAN>
2023-01-04 22:40:13 +0100

    Shop

--- Lamda/src/Main.java
+++ Lamda/src/Main.java
@@ -1,11 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         String name = "Ich";
-        MyInterface myInterface = (x, y) -> {
-            System.out.println("Hello World");
-            System.out.println("I am the best");
-            System.out.println("Hey " + x + " You are " + y);
-        };
+        int a = 1;
+        MyInterface myInterface;
+        if(a == 1) {
+            myInterface = (x, y) -> {
+                System.out.println("Hello World");
+                System.out.println("I am the best");
+                System.out.println("Hey " + x + " You are " + y);
+            };
+        }
+        else{
+            myInterface = (x, y) -> {
+                System.out.println("Hello World");
+                System.out.println("I am the best");
+                System.out.println("Hey " + x + " You are " + y);
+            };
+        }
         myInterface.showInfo(name, 39);
     }
 }
